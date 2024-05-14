23c2eeb95934085e107f92c68136916d4db62c04
linh2020 <cdev.linh@gmail.com>
2024-05-14 01:42:28 -0700

    Updated Error-handling middleware

--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,4 +1,4 @@
 const notFoundMiddleware = (req, res, next) =>
-  res.status(404).json({ msg: "Routes does not exist." });
+  res.status(404).json({ msg: "Route does not exist" });
 
 module.exports = notFoundMiddleware;

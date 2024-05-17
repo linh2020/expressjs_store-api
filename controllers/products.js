47a2bcdad5b81aade6499a3df4c3caf43f9396d3
linh2020 <cdev.linh@gmail.com>
2024-05-17 02:33:11 -0700

    Sort - getAllProducts Implementation - sorting with multiple conditions.

--- controllers/products.js
+++ controllers/products.js
@@ -7,7 +7,7 @@ const getAllProductStatic = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
   // console.log(req.query); // { name: 'john', feature: 'true' }
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -22,9 +22,17 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: "i" };
   }
 
-  console.log(queryObject); // { featured: false }, {}, etc.
+  // console.log(queryObject); // { featured: false }, {}, etc.
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    console.log(sortList);
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createAt");
+  }
 
-  const products = await Product.find(queryObject);
+  const products = await result;
 
   res.status(200).json({ products, nbHits: products.length });
 };
